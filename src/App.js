3ad14d121c0679ab07f9fd4f9eced69b5091088e
pelumi454 <amoochristianah454@gmail.com>
2021-02-15 20:30:48 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import About from './Components/About/About'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './About.scss'
+
+const About = () => {
+    return (
+        <div className="about"> 
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi deserunt eligendi officiis perferendis, harum modi accusamus deleniti, commodi velit sunt nulla est quasi necessitatibus. Harum excepturi expedita assumenda laudantium veniam sed error autem totam! Quisquam esse consectetur expedita explicabo quos consequatur culpa rem repudiandae id, recusandae laborum eaque, perferendis ex. Vel minima omnis nam possimus, pariatur, numquam accusamus fugit recusandae voluptates earum cupiditate eligendi aut blanditiis quisquam, animi commodi itaque.</p>
+        </div>
+    )
+}
+
+export default About
