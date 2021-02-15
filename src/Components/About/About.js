0892f44beb815140234ce9c435a6a4ad4521395d
pelumi454 <amoochristianah454@gmail.com>
2021-02-15 22:26:33 +0000

    changes

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './About.scss'
+import twitter from '../../img/twitter.svg'
+import github from '../../img/github.svg'
+import linkedin from '../../img/linkedin.svg'
 
 const About = () => {
     return (
         <div className="about"> 
             <p className="about-p1">About Me</p>
             <p className="about-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi deserunt eligendi officiis perferendis, harum modi accusamus deleniti, commodi velit sunt nulla est quasi necessitatibus. Harum excepturi expedita assumenda laudantium veniam sed error autem totam! Quisquam esse consectetur expedita explicabo quos consequatur culpa rem repudiandae id, recusandae laborum eaque, perferendis ex. Vel minima omnis nam possimus, pariatur, numquam accusamus fugit recusandae voluptates earum cupiditate eligendi aut blanditiis quisquam, animi commodi itaque explicabo quos consequatur culpa rem repudiandae id, recusandae laborum eaque, perferendis ex. Vel minima omnis nam possimuspa.</p>
+            <div className="about-icons">
+                <img src={twitter} alt="twitter-icon"/>
+                <img src={github} alt="github-icon"/>
+                <img src={linkedin} alt="linkedin-icon"/>
+            </div>
         </div>
     )
 }
