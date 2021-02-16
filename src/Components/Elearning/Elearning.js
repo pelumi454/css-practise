7502520f969ccd8ec8430e9026109f3fbd06b45e
pelumi454 <amoochristianah454@gmail.com>
2021-02-16 00:39:29 +0000

    changes

--- src/Components/Elearning/Elearning.js
+++ src/Components/Elearning/Elearning.js
@@ -7,7 +7,7 @@ const Elearning = () =>  {
         <div className="elearning">
             <img src={elearn} alt="library-img" className="elearning-img"/>
             <h4>An Introduction into information technology</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget lacus tellus. Etiam nec sem nibh. Nullam pulvinar quis sapien at tincidunt. In elit quam, consectetur eget felis bibendum, egestas luctus risus. Etiam ut enim sapien. Ut dictum, mi id malesuada rhoncus, elit velit porttitor purus, sit amet lobortis elit dui libero. Praesent nec nibh quis felis faucibus molestie. Nullam vehicula purus quis dui commodo dapibus. Ut sed aliquam enim.</p>
+            <p className="elearning-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget lacus tellus. Etiam nec sem nibh. Nullam pulvinar quis sapien at tincidunt. In elit quam, consectetur eget felis bibendum, egestas luctus risus. Etiam ut enim sapien. Ut dictum, mi id malesuada rhoncus, elit velit porttitor purus, sit amet lobortis elit dui libero. Praesent nec nibh quis felis faucibus molestie. Nullam vehicula purus quis dui commodo dapibus. Ut sed aliquam enim .</p>
         </div>
     )
 }
